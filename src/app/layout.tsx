05c5acbf38443042db83d7f13e26094f43ace46e
zagob <matheusbestana@gmail.com>
2025-04-07 15:49:16 -0300

    initial project

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PrepWise",
+  description: "An AI-powered platform for preparing for interviews.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monaSans.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <p>This is a sample page.</p>
+    </div>
+  );
+}
+
+export default Page
